2c179396bba4316dcfe0b2a4d10cf515c2e10c94
Jordan Pearson <JSantiPearson@gmail.com>
2020-11-10 16:07:31 -0800

    Add more feats

--- CreateComponents/Builder/featScreen.js
+++ CreateComponents/Builder/featScreen.js
@@ -113,8 +113,8 @@ const brace = {
 const comboKing = {
   name: "Combo King",
   pre: [["feat", "teamPlayer", 1], ["level", 35]],
-  description: "You can combine your own moves together."
-  tierOne: "You can combine moves at star cost 3 and below."
+  description: "You can combine your own moves together.",
+  tierOne: "You can combine moves at star cost 3 and below.",
   tierTwo: "You can combine moves at star cost 5 and below."
 }
 
@@ -163,7 +163,7 @@ const culturalChameleon = {
 
 const deduction = {
   name: "Deduction",
-  description: "You can use your Investigation bonus in place of Detection, but only ever as having a single level of training. (so if even if you have a 40 to Investigation but no training in Detection, then you may only receive perks of 1 level of training in Detection.)"
+  description: "You can use your Investigation bonus in place of Detection, but only ever as having a single level of training. (Eg. if you have a 40 to Investigation but no training in Detection, then you may only receive perks of 1 level of training in Detection.)"
 }
 
 const defang = {
@@ -190,6 +190,246 @@ const delicateFighter = {
 const dexterousDisarm = {
   name: "Dexterous Disarm",
   description: "You can substitute your Strength bonus for your Dexterity bonus to attempt a disarm, without needing to make a grapple check. You and your target roll contested Dexterity for Disarm checks made this way. If you have this feat, you become immune to this feat."
+}
+
+const dodgeCounter = {
+  name: "Dodge Counter",
+  pre: [["level", 10]],
+  description: "If you successfully dodge a melee attack, you can make a counter attack against your attacker. This can be done a number of times equal to your tier of play every long rest. Subsequent uses of this feat inflict levels of exhaustion until you take a long rest."
+}
+
+const disguisedStrike = {
+  name: "Disguised Strike",
+  pre: [["level", 25]],
+  description: "You can roll a stealth check to hide one standard action. You will still have to openly declare your action, but no creatures are allowed to reflexively react to it. Attacks of Opportunity and Counter Attacks can’t be made in response to it.",
+  tierOne: "You can make a DC 50 Stealth check once per set to hide a standard action. +15 is added to the DC for every enemy in the initiative after the first.",
+  tierTwo: "You can make a DC 50 Stealth check twice per set to hide a standard action. +10 is added to the DC for every enemy in the initiative after the first.",
+  tierThree: "You can make a DC 50 Stealth check once per round to hide a standard action. +5 is added to the DC for every enemy in the initiative after the first.",
+}
+
+const environmentalAdaptation = {
+  name: "Environmental Adaptation",
+  description: "You can handle the sudden change of environment with varying degrees of ease.",
+  tierOne: "You gain a +10 bonus when resisting Burn and Frostbite.",
+  tierTwo: "You gain a +20 bonus when resisting Burn and Frostbite.",
+  tierThree: "You gain a +40 bonus when resisting Burn and Frostbite."
+}
+
+const equilibrium = {
+  name: "Equilibrium",
+  description: "You always land on your feet after falling or being thrown. You don’t go prone from standard maneuvers."
+}
+
+const escapeArtist = {
+  name: "Escape Artist",
+  description: "You can find a weakness in any prison.",
+  tierOne: "You gain a +10 bonus to all checks made to escape a physical constraint.",
+  tierTwo: "You gain a +20 bonus to all checks made to escape a physical constraint."
+}
+
+const exoticWeaponUser = {
+  name: "Exotic Weapon User",
+  description: "You can use a complex and wild weapon.",
+  tierOne: "You can barely use this weapon. Subtract 20 from your base attack when using this weapon.",
+  tierTwo: "You can understand the fundamentals of this weapon. Subtract 10 from your base attack when using this weapon.",
+  tierThree: "You can use this weapon competently. Use your base attack when using this weapon.",
+  tierFour: "You can use this weapon effectively. Add 15 to your base attack when using this weapon.",
+  tierFive: "You have mastered this weapon. Add 25 to your base attack when using this weapon."
+}
+
+const everyman = {
+  name: "Everyman",
+  description: "You can use moves in the Mundane genre."
+}
+
+const expertInitiative = {
+  name: "Expert Initiative",
+  description: "You gain advantage on all initiative rolls."
+}
+
+const fakeOut = {
+  name: "Fake Out",
+  description: "You can make a fake attack against an enemy's defenses. On a success, you can make an attack against another target within range. They must roll their defense flat-footed. This feat can be used once per encounter."
+}
+
+const fastResponse = {
+  name: "Fast Response",
+  description: "Once per encounter, you can reflexively counter attack any creature slower than you."
+}
+
+const fighter = {
+  name: "Fighter",
+  description: "You are an avid combatant, and can enter all fights without being stunned. You don't have to reroll initiative after taking a breather or at the end of a set."
+}
+
+//TODO: This feat will likely get some changes to it.
+const flak = {
+  name: "Flak",
+  description: "You can treat grenades as precision attacks against yourself. Non-AoE capital weapons aren’t considered AoE against you."
+}
+
+const flinch = {
+  name: "Flinch",
+  description: "You gain an Attack of Opportunity on any visible creature that is stunned within your range. Stacks with similar feats."
+}
+
+const fullFortitude = {
+  name: "Full Fortitude",
+  pre: [["level", 10]],
+  description: "You may forgo your Reflex Save to double your Fortitude Save for the rest of an encounter. You may still only use your Fortitude Save to defend against one attack in a round."
+}
+
+const focus = {
+  name: "Focus",
+  description: "You can hone your mind to concentrate on one thing. Distractions are half as effective. You can spend consecutive standard actions to add +10 per standard action to a single attack on a single target. This feat can stack until the end of the current set, but when the set ends, all bonuses disappear. Additionally, Focus prevents exhaustion from straining an action once per long rest."
+}
+
+const guardCounter = {
+  name: "Guard Counter",
+  pre: [["level", 10]],
+  description: "If you successfully block a melee attack, you can make a counter attack against your attacker. This can be done a number of times equal to your tier of play every long rest. Subsequent uses of this feat inflict levels of exhaustion until you take a long rest."
+}
+
+const gentleFighter = {
+  name: "Gentle Fighter",
+  description: "You can remove Murderous Intent from any attack you make."
+}
+
+const greaseMonkey = {
+  name: "Grease Monkey",
+  description: "You can use moves in the Tech genre."
+}
+
+const grenadier = {
+  name: "Grenadier",
+  tierOne: "You can double the range of thrown grenades.",
+  tierTwo: "You can treat grenades as unblockable non-AOE precision attacks against all creatures in the range of the explosion."
+}
+
+const guerilla = {
+  name: "Guerilla",
+  pre: [["level", 25]],
+  description: "You can attack from stealth without breaking Stealth or re-rolling it."
+}
+
+const headstrong = {
+  name: "Headstrong",
+  pre: [["level", 25], ["block", 25]],
+  description: "You double your Armor when blocking attacks from the front or above you. This feat only works if you haven't moved this round, and doesn’t work against AoE."
+}
+
+const hearty = {
+  name: "Hearty",
+  pre: [["constitution", 14]],
+  description: "You have terrific health and a sturdy immune system. It takes twice as long for poison to inflict a check, and all poison damage is halved."
+}
+
+const higherMind = {
+  name: "Higher Mind",
+  description: "You can use moves in the Psychic genre."
+}
+
+//TODO: Figure out how to implement multi choice feats like this.
+const homeFieldAdvantage = {
+  name: "Home-Field Advantage",
+  description: "Haven't worked out how to implement this one yet."
+}
+
+const hook = {
+  name: "Hook",
+  pre: [["level", 10]],
+  definition: "You can use Climb to throw enemies without needing to roll to grapple them first. For all other effects, this still counts as a grapple."
+}
+
+const hopTo = {
+  name: "Hop To",
+  pre: [["level", 10], ["dexterity", 12]],
+  definition: "You can return to your feet from a prone position as a reflexive action."
+}
+
+const improvFighter = {
+  name: "Improv Fighter",
+  description: "You can treat any object you can carry as a weapon with which you have proficiency. This can only be done once a long rest. This does not apply to actual weapons (does work on a 2x4, doesn’t work on a baseball bat)."
+}
+
+const intuition = {
+  name: "Intuition",
+  description: "You can use your Detection bonus in place of Investigation, but only ever as having a single level of training. (Eg. if you have a 40 to Detection but no training in Investigation, then you may only receive perks of 1 level of training in Investigation."
+}
+
+const ironLung = {
+  name: "Iron Lung",
+  description: "You have the ability to control your breath while asphyxiating. This feat does not take effect against strangulation.",
+  tierOne: "You gain additional minute of air before you must make a Fortitude Save.",
+  tierTwo: "You gain two additional minutes of air before you must make a Fortitude Save.",
+  tierThree: "You gain three additional minutes of air before you must make a Fortitude Save."
+}
+
+const jitter = {
+  name: "Jitter",
+  description: "You treat friendly AoE as partial cover."
+}
+
+const killerInstinct = {
+  name: "Killer Instinct",
+  description: "You can add Murderous Intent to any attack you make."
+}
+
+const leadArm = {
+  name: "Lead Arm",
+  pre: [["level", 10]],
+  description: "You can wield size 2 weapons one-handed."
+}
+
+const liftKick = {
+  name: "Lift Kick",
+  description: "You can make a basic melee attack while prone. If it lands, you will stand to your feet and knock your target prone. On a failure, your target may attack you for free. If you make a basic melee attack after jumping, your damage is doubled and you travel another 2 squares into the air."
+}
+
+const loopHole = {
+  name: "Loop Hole",
+  description: "Your attack bonuses (not damage) double against targets using Akimbo Fighter, if they have two or more weapons equipped."
+}
+
+const lowCounter = {
+  name: "Low Counter",
+  description: "You perform a low kick on any adjacent creature. That creature may choose to forego any attacks declared against you. If they do not, they must make a Reflex Save against a DC equal to your melee stat (without base attack) +10 per level of martial arts. If your target fails, they are knocked prone. At level 25, you may instantly trip all adjacent creatures once per encounter."
+}
+
+const masochist = {
+  name: "Masochist",
+  pre: [["not species", "construct"]], /* TODO: Implement "not species" prerequisite */
+  description: "You convert your TH into HP for a base armor bonus of +10."
+}
+
+const martialArts = {
+  name: "Martial Arts",
+  description: "You have trained in a dangerous fighting style.",
+  tierOne: "You are a novice. Add an additional die of damage to successful melee attacks.",
+  tierTwo: "You are proficient. Add two additional dice of damage to successful melee attacks."
+  tierThree: "You are a master. Add three additional dice of damage to successful melee attacks."
+}
+
+const multiLimb = {
+  name: "Multi-limb",
+  pre: [["level", 20], ["species", "construct", "energybeing"]],
+  tierOne: "You have built or formed an additional limb, and can use it to equip or hold items.",
+  tierTwo: "Your size increases by 1, and you gain another limb."
+}
+
+const naturalLeader = {
+  name: "Natural Leader",
+  description: "You can motivate and inspire your allies by giving a rousing speech. Using this feat more than once per encounter inflicts exhaustion.",
+  tierOne: "All adjacent allies with a lower Influence score than you recovers 6d6 TH.",
+  tierTwo: "All adjacent allies with a lower Influence score than you recovers 12d6 TH.",
+}
+
+const nemesis = {
+  name: "Nemesis",
+  pre: [["level", 20]],
+  definition: "You have a sworn enemy. If you take this feat, your enemy will obtain it as well. If you convert your nemesis to your side, or vice versa, this feat is replaced with the Companion feat at the same level. You can only ever declare one nemesis at a time.",
+  tierOne: "You and your foe gain a +10 to all combat rolls while in the same initiative. You gain advantage on skills you have training in when nearby your enemy, unless your enemy is also trained in that skill.",
+  tierTwo: "Neither you nor your foe can inflict stun on the other, and the +10 bonus doubles to +20 but now applies to your allies and all of the allies of your foe as well.]"
 }
 
 class Feats extends Component {
